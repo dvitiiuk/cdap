2dd2dc9531a57bf9982fdd23f1f4af000e065524
Dmytro Vitiuk <dvitiiuk@cybervisiontech.com>
2023-06-13 16:29:32 +0300

    Single-line logging

---
+++ cdap-common/src/main/java/io/cdap/cdap/common/SingleLineThrowableConverter.java
@@ -0,0 +1,37 @@
+package io.cdap.cdap.common;
+
+import ch.qos.logback.classic.pattern.ThrowableProxyConverter;
+import ch.qos.logback.classic.spi.IThrowableProxy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SingleLineThrowableConverter extends ThrowableProxyConverter {
+  private String regex = "\n";
+  private String replacement = "|";
+
+  @Override
+  public void start() {
+    List<String> options = getOptionList();
+    if (options != null && !options.isEmpty()) {
+
+      options = new ArrayList<>(getOptionList());
+      if (options.size() == 1) {
+        replacement = options.remove(0);
+      } else {
+        regex = options.remove(0);
+        replacement = options.remove(0);
+      }
+      super.setOptionList(options);
+    }
+
+    super.start();
+  }
+
+  @Override
+  protected String throwableProxyToString(IThrowableProxy tp) {
+    String original = super.throwableProxyToString(tp);
+
+    return original.replaceAll(regex, replacement);
+  }
+}
